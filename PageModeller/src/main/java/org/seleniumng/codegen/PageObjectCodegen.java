28d74c0024d267420f6b8a399693a7a6bd20dccc
Niranjan Khare <niranjankhare@gmail.com>
2018-04-10 11:01:00 +0530

    write page classes and resources; wip

--- PageModeller/src/main/java/org/seleniumng/codegen/PageObjectCodegen.java
+++ PageModeller/src/main/java/org/seleniumng/codegen/PageObjectCodegen.java
@@ -19,23 +19,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
-import java.util.jar.JarException;
 
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
-import com.sun.codemodel.JEnumConstant;
 import com.sun.codemodel.JExpr;
-import com.sun.codemodel.JExpression;
-import com.sun.codemodel.JFieldVar;
-import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
+import com.sun.codemodel.JPackage;
+import com.sun.codemodel.fmt.JTextFile;
 
 import static org.seleniumng.utils.TAFConfig.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 import org.seleniumng.controls.*;
@@ -43,6 +39,9 @@ import org.seleniumng.ui.LibDatabase;
 
 public class PageObjectCodegen {
 	private static JCodeModel codeModel = new JCodeModel();
+	private static JCodeModel resourceModel = new JCodeModel();
+	private static String sourceDirPath = "src/main/java";
+	private static String resourceDirPath = "src/main/resources";
 	public static void main(String... args) {
 
 		try {
@@ -63,70 +62,11 @@ public class PageObjectCodegen {
 		List<String> webPages = fetchPageList();
 		for (String webPage : webPages) {
 			JDefinedClass pageClassToCreate = generatePageObject(pagePackage, webPage);
-			// JClass jc = codeModel.ref(TextField.class);
-			//
-			// // Creating fields in the class
-			//
-			// JFieldVar field1 = pageClassToCreate.field(JMod.PRIVATE, jc,
-			// "foo", JExpr._null());
-
 			repositoryToCreate.field(JMod.PUBLIC, pageClassToCreate, "page" + webPage);
 
 		}
-		// again
 
-		// JDefinedClass will let you create a class in a specified package.
-
-		/*
-		 * //The codeModel instance will have a list of Java primitives which
-		 * can be //used to create a primitive field in the new class JFieldVar
-		 * field2 = classToBeCreated.field(JMod.PRIVATE, codeModel.DOUBLE,
-		 * "bar",JExpr.direct("new TextField()")); // field2.assign( ); //Create
-		 * getter and setter methods for the fields JMethod field1GetterMethod =
-		 * classToBeCreated.method(JMod.PUBLIC, field1.type(), "getFoo"); //code
-		 * to create a return statement with the field1
-		 * field1GetterMethod.body()._return(field1); JMethod field1SetterMethod
-		 * = classToBeCreated.method(JMod.PUBLIC, codeModel.VOID, "setFoo");
-		 * //code to create an input parameter to the setter method which will
-		 * take a variable of type field1
-		 * field1SetterMethod.param(field1.type(), "inputFoo"); //code to create
-		 * an assignment statement to assign the input argument to the field1
-		 * field1SetterMethod.body().assign(JExpr._this().ref ("foo"), JExpr.ref
-		 * ("inputFoo"));
-		 * 
-		 * JMethod field2GetterMethod = classToBeCreated.method(JMod.PUBLIC,
-		 * field2.type(), "getBar"); field2GetterMethod.body()._return(field2);
-		 * JMethod field2SetterMethod = classToBeCreated.method(JMod.PUBLIC,
-		 * codeModel.VOID, "setBar"); field2SetterMethod.param(field2.type(),
-		 * "inputBar"); field2SetterMethod.body().assign(JExpr._this().ref
-		 * ("bar"), JExpr.ref ("inputBar"));
-		 * 
-		 * //creating an enum class within our main class JDefinedClass
-		 * enumClass = classToBeCreated._enum(JMod.PUBLIC, "REPORT_COLUMNS");
-		 * //This code creates field within the enum class JFieldVar columnField
-		 * = enumClass.field(JMod.PRIVATE|JMod.FINAL, String.class, "column");
-		 * JFieldVar filterableField = enumClass.field(JMod.PRIVATE|JMod.FINAL,
-		 * codeModel.BOOLEAN, "filterable");
-		 * 
-		 * //Define the enum constructor JMethod enumConstructor =
-		 * enumClass.constructor(JMod.PRIVATE);
-		 * enumConstructor.param(String.class, "column");
-		 * enumConstructor.param(codeModel.BOOLEAN, "filterable");
-		 * enumConstructor.body().assign(JExpr._this().ref ("column"),
-		 * JExpr.ref("column")); enumConstructor.body().assign(JExpr._this().ref
-		 * ("filterable"), JExpr.ref("filterable"));
-		 * 
-		 * JMethod getterColumnMethod = enumClass.method(JMod.PUBLIC,
-		 * String.class, "getColumn");
-		 * getterColumnMethod.body()._return(columnField); JMethod
-		 * getterFilterMethod = enumClass.method(JMod.PUBLIC, codeModel.BOOLEAN,
-		 * "isFilterable"); getterFilterMethod.body()._return(filterableField);
-		 * 
-		 * JEnumConstant enumConst = enumClass.enumConstant("FOO_BAR");
-		 * enumConst.arg(JExpr.lit("fooBar")); enumConst.arg(JExpr.lit(true));
-		 */
-		// This will generate the code in the specified file path.
-		codeModel.build(new File("src/main/java"));
+		codeModel.build(new File(sourceDirPath));
 	}
 
 	private static List<String> fetchPageList() {
@@ -141,8 +81,11 @@ public class PageObjectCodegen {
 	private static JDefinedClass generatePageObject(String pPackage, String webPage) throws IOException {
 		
 		JDefinedClass retClass = null;
+		JPackage retResource  = null;
 		try {
 			retClass = codeModel._class(pPackage + ".Page" + webPage);
+			retResource = resourceModel._package(pPackage+".en");
+
 		} catch (Exception e) {
 
 		}
@@ -156,8 +99,70 @@ public class PageObjectCodegen {
 			
 			JClass jc = codeModel.ref("org.seleniumng.controls."+customClass);
 			retClass.field(JMod.PRIVATE, jc, classAbrv+control, JExpr._null());
+			
 		}
-		codeModel.build(new File("src/main/java"));
+		String rsrcPath = "Page" + webPage+".conf";
+		JTextFile rsrc = new JTextFile(rsrcPath);
+		rsrc.setContents("resourceContent");
+		retResource.addResourceFile(rsrc );
+		resourceModel.build(new File(resourceDirPath));
 		return retClass;
 	}
+
+	// again
+
+	// JDefinedClass will let you create a class in a specified package.
+
+	/*
+	 * //The codeModel instance will have a list of Java primitives which
+	 * can be //used to create a primitive field in the new class JFieldVar
+	 * field2 = classToBeCreated.field(JMod.PRIVATE, codeModel.DOUBLE,
+	 * "bar",JExpr.direct("new TextField()")); // field2.assign( ); //Create
+	 * getter and setter methods for the fields JMethod field1GetterMethod =
+	 * classToBeCreated.method(JMod.PUBLIC, field1.type(), "getFoo"); //code
+	 * to create a return statement with the field1
+	 * field1GetterMethod.body()._return(field1); JMethod field1SetterMethod
+	 * = classToBeCreated.method(JMod.PUBLIC, codeModel.VOID, "setFoo");
+	 * //code to create an input parameter to the setter method which will
+	 * take a variable of type field1
+	 * field1SetterMethod.param(field1.type(), "inputFoo"); //code to create
+	 * an assignment statement to assign the input argument to the field1
+	 * field1SetterMethod.body().assign(JExpr._this().ref ("foo"), JExpr.ref
+	 * ("inputFoo"));
+	 * 
+	 * JMethod field2GetterMethod = classToBeCreated.method(JMod.PUBLIC,
+	 * field2.type(), "getBar"); field2GetterMethod.body()._return(field2);
+	 * JMethod field2SetterMethod = classToBeCreated.method(JMod.PUBLIC,
+	 * codeModel.VOID, "setBar"); field2SetterMethod.param(field2.type(),
+	 * "inputBar"); field2SetterMethod.body().assign(JExpr._this().ref
+	 * ("bar"), JExpr.ref ("inputBar"));
+	 * 
+	 * //creating an enum class within our main class JDefinedClass
+	 * enumClass = classToBeCreated._enum(JMod.PUBLIC, "REPORT_COLUMNS");
+	 * //This code creates field within the enum class JFieldVar columnField
+	 * = enumClass.field(JMod.PRIVATE|JMod.FINAL, String.class, "column");
+	 * JFieldVar filterableField = enumClass.field(JMod.PRIVATE|JMod.FINAL,
+	 * codeModel.BOOLEAN, "filterable");
+	 * 
+	 * //Define the enum constructor JMethod enumConstructor =
+	 * enumClass.constructor(JMod.PRIVATE);
+	 * enumConstructor.param(String.class, "column");
+	 * enumConstructor.param(codeModel.BOOLEAN, "filterable");
+	 * enumConstructor.body().assign(JExpr._this().ref ("column"),
+	 * JExpr.ref("column")); enumConstructor.body().assign(JExpr._this().ref
+	 * ("filterable"), JExpr.ref("filterable"));
+	 * 
+	 * JMethod getterColumnMethod = enumClass.method(JMod.PUBLIC,
+	 * String.class, "getColumn");
+	 * getterColumnMethod.body()._return(columnField); JMethod
+	 * getterFilterMethod = enumClass.method(JMod.PUBLIC, codeModel.BOOLEAN,
+	 * "isFilterable"); getterFilterMethod.body()._return(filterableField);
+	 * 
+	 * JEnumConstant enumConst = enumClass.enumConstant("FOO_BAR");
+	 * enumConst.arg(JExpr.lit("fooBar")); enumConst.arg(JExpr.lit(true));
+	 */
+	
+
+
 }
+
