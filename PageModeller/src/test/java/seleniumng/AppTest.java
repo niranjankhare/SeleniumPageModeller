e4dd052a25cf155a7328fe1c39c73f5bf65a707c
Niranjan Khare <niranjankhare@gmail.com>
2018-05-07 06:04:29 +0530

    read config  wip3

--- PageModeller/src/test/java/seleniumng/AppTest.java
+++ PageModeller/src/test/java/seleniumng/AppTest.java
@@ -2,6 +2,13 @@ package seleniumng;
 
 
 import static myAut.PageLibraries.PageRepository.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import myAut.PageLibraries.PageRepository;
 
 public class AppTest {
@@ -19,5 +26,19 @@ public class AppTest {
 //		System.out.println(pageLogin.controlAtTheBase.friendlyName);
 
 	}
+	
+	@Test
+	public void testGoogleSearch() throws InterruptedException {
+	  // Optional, if not specified, WebDriver will search your path for chromedriver.
+	  System.setProperty("webdriver.chrome.driver", "D:\\Users\\niru\\Sandbox\\chromedriver.exe");
 
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www.google.com/xhtml");
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  WebElement searchBox = driver.findElement(By.name("q"));
+	  searchBox.sendKeys("ChromeDriver");
+	  searchBox.submit();
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  driver.quit();
+	}
 }
