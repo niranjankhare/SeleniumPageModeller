940307fe5a7adc1490a01a4b562c79bf7052c9d1
Niranjan Khare <niranjankhare@gmail.com>
2018-05-22 19:39:18 +0530

    SessionManagement change..

---
+++ PageModeller/src/main/java/org/seleniumng/driver/SessionInventory.java
@@ -0,0 +1,258 @@
+/*******************************************************************************
+ * Copyright 2018 Niranjan Khare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.seleniumng.driver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.seleniumng.utils.TAFConfig;
+
+import com.typesafe.config.Config;
+/**
+ * Class to manage webdriver sessions.. for now based only on thread
+ * but would need it to manage multiple webdriver sessions for a single thread
+ * @author niru
+ *
+ */
+public class SessionInventory {
+	private static Config gridHub = TAFConfig.getConfig("seleniumGrid");
+	private static LinkedHashMap<SessionId, WebDriver> sessionInventory = new LinkedHashMap<SessionId, WebDriver>();
+
+	public static WebDriver getDriver(SessionId session) {
+		System.setProperty("webdriver.chrome.driver", TAFConfig.sysPropChromeDriverPath);
+		System.setProperty("webdriver.gecko.driver", TAFConfig.sysPropMozGeckoDriverPath);
+
+		WebDriver driver = sessionInventory.get(session);
+		if (driver == null) {
+			if (gridHub == null)
+				return getDriver(Thread.currentThread().getId());
+			else {
+				return setWebDriver(BrowserType.GOOGLECHROME);
+			}
+		} else
+			return driver;
+
+	}
+
+	private static WebDriver getDriver(Long threadId) {
+		WebDriver driver = sessionInventory.get(threadId);
+		if (driver == null) {
+			// driver =new ChromeDriver();
+
+			driver = new ChromeDriver();
+			SessionId session = ((ChromeDriver)driver).getSessionId();
+			sessionInventory.put(session, driver);
+			System.out.println("Returning new instance!!");
+		} else {
+			System.out.println("Returning Existing instance!!");
+		}
+		return driver;
+	}
+
+	/**
+	 * Sets up the browser to be used for running automation
+	 * 
+	 * @param browser
+	 *            : See {@link BrowserType} This method is most probably not
+	 *            goign to be used, as we will need some additional param to
+	 *            specify profile related to localized language etc. See
+	 *            setWebDriver (Browser, profile)
+	 */
+	private static WebDriver setWebDriver(String browser) {
+		DesiredCapabilities desiredCapabilities = null;
+		ChromeOptions options = null;
+		switch (browser) {
+		case BrowserType.FIREFOX:
+			FirefoxProfile profile = new FirefoxProfile();
+			profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv");
+			desiredCapabilities = DesiredCapabilities.firefox();
+			desiredCapabilities.setCapability(FirefoxDriver.PROFILE, profile);
+
+			break;
+		case BrowserType.GOOGLECHROME:
+			// desiredCapabilities = DesiredCapabilities.chrome();
+			options = new ChromeOptions();
+			// options.addArguments("--test-type");
+			options.setCapability(CapabilityType.PLATFORM_NAME, Platform.LINUX);
+			options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.ACCEPT);
+			options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			// desiredCapabilities.setCapability(ChromeOptions.CAPABILITY,
+			// options);
+
+			break;
+		// case IE:
+		// desiredCapabilities = DesiredCapabilities.internetExplorer();
+		// break;
+		}
+		// return setWebDriver (browser, desiredCapabilities);
+		return setWebDriver(browser, options);
+
+	}
+
+	private static WebDriver setWebDriver(String browser, ChromeOptions options) {
+		WebDriver webDriver = null;
+		// if (SuiteConfiguration.useGrid){
+		String remoteDriverUrlString;
+		if (!gridHub.getString("port").equals("")) {
+			remoteDriverUrlString = "http://" + gridHub.getString("host") + ":" + gridHub.getString("port") + "/wd/hub";
+		} else {
+			remoteDriverUrlString = "http://" + gridHub.getString("host") + "/wd/hub";
+		}
+		URL remoteDriverUrl = null;
+		try {
+			remoteDriverUrl = new URL(remoteDriverUrlString);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		webDriver = new RemoteWebDriver(remoteDriverUrl, options);
+		((RemoteWebDriver) webDriver).setFileDetector(new LocalFileDetector());
+		// }
+		// else{
+		//
+
+		/***
+		 * To Remove
+		 */
+		//
+		// switch (browser.toString()){
+		// case BrowserType.FIREFOX:
+		// webDriver = new FirefoxDriver(desiredCapabilities);
+		// break;
+		//// case IE:
+		//// System.setProperty("webdriver.ie.driver",
+		// SuiteConfiguration.sysPropIEDriverPath);
+		//// webDriver = new InternetExplorerDriver(desiredCapabilities);
+		//// break;
+		// case BrowserType.GOOGLECHROME:
+		// System.setProperty("webdriver.chrome.driver",
+		// TAFConfig.sysPropChromeDriverPath);
+		// webDriver = new ChromeDriver(desiredCapabilities);
+		// break;
+		// }
+		//
+		/***
+		 * To Remove ^^^^^^
+		 */
+
+		//
+		// }
+		webDriver.manage().timeouts().implicitlyWait(TAFConfig.DEFAULT_IMPLICIT_WAIT, TimeUnit.SECONDS);
+		Dimension d = new Dimension(1280, 1024);
+		// Resize the current window to the given dimension
+
+		// if (!SuiteConfiguration.fullscreen) {
+		webDriver.manage().window().setSize(d);
+		// }else {
+		// webDriver.manage().window().maximize();
+		// }
+		 SessionId session = ((RemoteWebDriver)webDriver).getSessionId();
+		sessionInventory.put(session, webDriver);
+		return webDriver;
+
+	}
+
+	/**
+	 * Sets up the Webdriver for running automation using the supplied browser
+	 * type and Profile
+	 * 
+	 * @param browser
+	 *            : See {@link BrowserType}
+	 * @param ffProfilePath
+	 */
+	private static WebDriver setWebDriver(String browser, DesiredCapabilities desiredCapabilities) {
+
+		WebDriver webDriver = null;
+		// if (SuiteConfiguration.useGrid){
+		String remoteDriverUrlString;
+		if (!gridHub.getString("port").equals("")) {
+			remoteDriverUrlString = "http://" + gridHub.getString("host") + ":" + gridHub.getString("port") + "/wd/hub";
+		} else {
+			remoteDriverUrlString = "http://" + gridHub.getString("host") + "/wd/hub";
+		}
+		URL remoteDriverUrl = null;
+		try {
+			remoteDriverUrl = new URL(remoteDriverUrlString);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		webDriver = new RemoteWebDriver(remoteDriverUrl, desiredCapabilities);
+		((RemoteWebDriver) webDriver).setFileDetector(new LocalFileDetector());
+		// }
+		// else{
+		//
+
+		/***
+		 * To Remove
+		 */
+		//
+		// switch (browser.toString()){
+		// case BrowserType.FIREFOX:
+		// webDriver = new FirefoxDriver(desiredCapabilities);
+		// break;
+		//// case IE:
+		//// System.setProperty("webdriver.ie.driver",
+		// SuiteConfiguration.sysPropIEDriverPath);
+		//// webDriver = new InternetExplorerDriver(desiredCapabilities);
+		//// break;
+		// case BrowserType.GOOGLECHROME:
+		// System.setProperty("webdriver.chrome.driver",
+		// TAFConfig.sysPropChromeDriverPath);
+		// webDriver = new ChromeDriver(desiredCapabilities);
+		// break;
+		// }
+		//
+		/***
+		 * To Remove ^^^^^^
+		 */
+
+		//
+		// }
+		webDriver.manage().timeouts().implicitlyWait(TAFConfig.DEFAULT_IMPLICIT_WAIT, TimeUnit.SECONDS);
+		Dimension d = new Dimension(1280, 1024);
+		// Resize the current window to the given dimension
+
+		// if (!SuiteConfiguration.fullscreen) {
+		webDriver.manage().window().setSize(d);
+		// }else {
+		// webDriver.manage().window().maximize();
+		// }
+		sessionInventory.put(((RemoteWebDriver) webDriver).getSessionId(), webDriver);
+		return webDriver;
+	}
+
+	public static void closeSession(SessionId session) {
+		Long sessionKey = Thread.currentThread().getId();
+		System.out.println("Closing Session:"+ sessionKey);
+		getDriver(session).close();
+		sessionInventory.remove(sessionKey);
+	}
+}
---
+++ PageModeller/src/main/java/org/seleniumng/utils/SessionManager.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright 2018 Niranjan Khare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.seleniumng.utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import org.seleniumng.driver.SessionInventory;
+
+public class SessionManager {
+
+	public SessionManager() {
+        Class<?> cls = this.getClass();
+        
+        Field[] fields = cls.getDeclaredFields();
+        
+        for (Field f:fields){
+            Class<?> ftype = f.getType();
+            Constructor<?> constructor = null;
+            try {
+                constructor = ftype.getConstructor();
+                Object o = constructor.newInstance();
+                f.set(this, o);
+                System.out.println("need to call method to assign session to 'o'");
+                assignSession (o);
+            } catch (InstantiationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+	private void assignSession(Object o) {
+		SessionInventory.getDriver();
+		
+	}
+	
+	
+	
+}
